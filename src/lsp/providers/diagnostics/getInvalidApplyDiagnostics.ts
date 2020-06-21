8c6ba844172f50a95583c34700903b3b03d382c7
Brad Cornes <bradlc41@gmail.com>
2020-06-21 01:38:08 +0100

    reuse @apply validation in hover provider

--- src/lsp/providers/diagnostics/getInvalidApplyDiagnostics.ts
+++ src/lsp/providers/diagnostics/getInvalidApplyDiagnostics.ts
@@ -2,7 +2,7 @@ import { findClassNamesInRange } from '../../util/find'
 import { InvalidApplyDiagnostic, DiagnosticKind } from './types'
 import { Settings, State } from '../../util/state'
 import { TextDocument, DiagnosticSeverity } from 'vscode-languageserver'
-import { getClassNameMeta } from '../../util/getClassNameMeta'
+import { validateApply } from '../../util/validateApply'
 
 export function getInvalidApplyDiagnostics(
   state: State,
@@ -15,38 +15,11 @@ export function getInvalidApplyDiagnostics(
   const classNames = findClassNamesInRange(document, undefined, 'css')
 
   let diagnostics: InvalidApplyDiagnostic[] = classNames.map((className) => {
-    const meta = getClassNameMeta(state, className.className)
-    if (!meta) return null
+    let result = validateApply(state, className.className)
 
-    let message: string
-
-    if (Array.isArray(meta)) {
-      message = `'@apply' cannot be used with '${className.className}' because it is included in multiple rulesets.`
-    } else if (meta.source !== 'utilities') {
-      message = `'@apply' cannot be used with '${className.className}' because it is not a utility.`
-    } else if (meta.context && meta.context.length > 0) {
-      if (meta.context.length === 1) {
-        message = `'@apply' cannot be used with '${className.className}' because it is nested inside of an at-rule ('${meta.context[0]}').`
-      } else {
-        message = `'@apply' cannot be used with '${
-          className.className
-        }' because it is nested inside of at-rules (${meta.context
-          .map((c) => `'${c}'`)
-          .join(', ')}).`
-      }
-    } else if (meta.pseudo && meta.pseudo.length > 0) {
-      if (meta.pseudo.length === 1) {
-        message = `'@apply' cannot be used with '${className.className}' because its definition includes a pseudo-selector ('${meta.pseudo[0]}')`
-      } else {
-        message = `'@apply' cannot be used with '${
-          className.className
-        }' because its definition includes pseudo-selectors (${meta.pseudo
-          .map((p) => `'${p}'`)
-          .join(', ')}).`
-      }
+    if (result === null || result.isApplyable === true) {
+      return null
     }
-
-    if (!message) return null
 
     return {
       code: DiagnosticKind.InvalidApply,
@@ -55,7 +28,7 @@ export function getInvalidApplyDiagnostics(
           ? DiagnosticSeverity.Error
           : DiagnosticSeverity.Warning,
       range: className.range,
-      message,
+      message: result.reason,
       className,
     }
   })
