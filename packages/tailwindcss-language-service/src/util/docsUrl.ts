4f0e415df778d1e95ca2f817b008979eb5a54247
Brad Cornes <bradlc41@gmail.com>
2020-11-27 17:05:58 +0000

    update docs urls

--- packages/tailwindcss-language-service/src/util/docsUrl.ts
+++ packages/tailwindcss-language-service/src/util/docsUrl.ts
@@ -2,9 +2,13 @@ import semver from 'semver'
 
 export function docsUrl(version: string, paths: string | string[]): string {
   let major = 0
-  let url = 'https://v0.tailwindcss.com/docs/'
+  let url = 'https://tailwindcss-v0.netlify.app/docs/'
   if (semver.gte(version, '0.99.0')) {
     major = 1
+    url = 'https://v1.tailwindcss.com/docs/'
+  }
+  if (semver.gte(version, '1.99.0')) {
+    major = 2
     url = 'https://tailwindcss.com/docs/'
   }
   const path = Array.isArray(paths)
