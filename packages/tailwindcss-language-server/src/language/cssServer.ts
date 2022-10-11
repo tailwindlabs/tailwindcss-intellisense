cccb8c6764530f34ea3ed5bf493634faf07055c0
Brad Cornes <hello@bradley.dev>
2022-10-11 10:32:58 +0100

    Tweak `theme()` completion

--- packages/tailwindcss-language-server/src/language/cssServer.ts
+++ packages/tailwindcss-language-server/src/language/cssServer.ts
@@ -162,10 +162,15 @@ connection.onCompletion(async ({ textDocument, position }, _token) =>
             {
               ...item,
               label: 'theme()',
+              filterText: 'theme',
               documentation: {
                 kind: 'markdown',
                 value:
                   'Use the `theme()` function to access your Tailwind config values using dot notation.',
+              },
+              command: {
+                title: '',
+                command: 'editor.action.triggerSuggest',
               },
               textEdit: {
                 ...item.textEdit,
