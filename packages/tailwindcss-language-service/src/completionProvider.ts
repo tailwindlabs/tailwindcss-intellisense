3e7890bb9bcee2a7ae5c27ffd0d9e95acca24dd3
Brad Cornes <hello@bradley.dev>
2022-10-19 18:16:56 +0100

    Prettier

--- packages/tailwindcss-language-service/src/completionProvider.ts
+++ packages/tailwindcss-language-service/src/completionProvider.ts
@@ -216,7 +216,10 @@ export function completionsFromClassList(
                 .filter((value) => !existingVariants.includes(`${variant.name}-${value}`))
                 .map((value) =>
                   variantItem({
-                    label: value === 'DEFAULT'? `${variant.name}${sep}` : `${variant.name}${variant.hasDash ? '-' : ''}${value}${sep}`,
+                    label:
+                      value === 'DEFAULT'
+                        ? `${variant.name}${sep}`
+                        : `${variant.name}${variant.hasDash ? '-' : ''}${value}${sep}`,
                     detail: variant.selectors({ value }).join(', '),
                   })
                 )
@@ -845,7 +848,9 @@ function provideVariantsDirectiveCompletions(
 
   let possibleVariants = state.variants.flatMap((variant) => {
     if (variant.values.length) {
-      return variant.values.map((value) => value === 'DEFAULT'? variant.name : `${variant.name}${variant.hasDash ? '-' : ''}${value}`)
+      return variant.values.map((value) =>
+        value === 'DEFAULT' ? variant.name : `${variant.name}${variant.hasDash ? '-' : ''}${value}`
+      )
     }
     return [variant.name]
   })
--- packages/tailwindcss-language-service/src/util/getVariantsFromClassName.ts
+++ packages/tailwindcss-language-service/src/util/getVariantsFromClassName.ts
@@ -7,7 +7,9 @@ export function getVariantsFromClassName(
 ): { variants: string[]; offset: number } {
   let allVariants = state.variants.flatMap((variant) => {
     if (variant.values.length) {
-      return variant.values.map((value) => value === 'DEFAULT'? variant.name : `${variant.name}${variant.hasDash ? '-' : ''}${value}`)
+      return variant.values.map((value) =>
+        value === 'DEFAULT' ? variant.name : `${variant.name}${variant.hasDash ? '-' : ''}${value}`
+      )
     }
     return [variant.name]
   })
