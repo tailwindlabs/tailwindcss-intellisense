33f94bae1a74621ca94ce62fa5053d64e6736203
Brad Cornes <hello@bradley.dev>
2023-03-26 18:20:24 +0100

    Fix use of certain built-in node modules in config file (#745)

--- packages/tailwindcss-language-server/src/lib/env.ts
+++ packages/tailwindcss-language-server/src/lib/env.ts
@@ -1,7 +1,7 @@
 import Module from 'module'
 import * as path from 'path'
 import resolveFrom from '../util/resolveFrom'
-import builtInModules from 'builtin-modules'
+import isBuiltinModule from 'is-builtin-module'
 
 process.env.TAILWIND_MODE = 'build'
 process.env.TAILWIND_DISABLE_TOUCH = 'true'
@@ -9,10 +9,7 @@ process.env.TAILWIND_DISABLE_TOUCH = 'true'
 let oldResolveFilename = (Module as any)._resolveFilename
 
 ;(Module as any)._resolveFilename = (id: any, parent: any) => {
-  if (
-    typeof id === 'string' &&
-    (builtInModules.includes(id) || builtInModules.includes(id.replace(/^node:/, '')))
-  ) {
+  if (typeof id === 'string' && isBuiltinModule(id)) {
     return oldResolveFilename(id, parent)
   }
   return resolveFrom(path.dirname(parent.id), id)
