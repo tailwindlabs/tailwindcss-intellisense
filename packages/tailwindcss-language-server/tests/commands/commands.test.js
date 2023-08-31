8d94ef77c8511b91ad66c073bcca693d74ede19b
Brad Cornes <hello@bradley.dev>
2023-08-31 10:58:15 +0100

    Add test

---
+++ packages/tailwindcss-language-server/tests/commands/commands.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from 'vitest'
+import { withFixture } from '../common'
+
+withFixture('basic', (c) => {
+  test.concurrent('sortSelection', async () => {
+    let textDocument = await c.openDocument({ text: '<div class="sm:p-0 p-0">' })
+    let res = await c.sendRequest('@/tailwindCSS/sortSelection', {
+      uri: textDocument.uri,
+      classLists: ['sm:p-0 p-0'],
+    })
+
+    expect(res).toEqual({ classLists: ['p-0 sm:p-0'] })
+  })
+})
