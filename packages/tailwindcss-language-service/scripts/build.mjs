3df69994c48b5b8df5fc06d24e43dccd716f9659
Jordan Pittman <jordan@cryptica.me>
2023-10-27 11:27:26 -0400

    Tweak build script

--- packages/tailwindcss-language-service/scripts/build.mjs
+++ packages/tailwindcss-language-service/scripts/build.mjs
@@ -1,6 +1,8 @@
-import esbuild from 'esbuild'
 import path from 'node:path'
+import { spawnSync } from 'node:child_process'
+import esbuild from 'esbuild'
 import minimist from 'minimist'
+import { nodeExternalsPlugin } from 'esbuild-node-externals'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
@@ -9,37 +11,43 @@ const args = minimist(process.argv.slice(2), {
 })
 
 console.log('- Preparing')
-let builds = await Promise.all([
-  esbuild.context({
-    entryPoints: [path.resolve(__dirname, '../src/index.ts')],
-    bundle: true,
-    platform: 'node',
-    external: [],
-    outdir: 'dist',
-    minify: args.minify,
+let build = await esbuild.context({
+  entryPoints: [path.resolve(__dirname, '../src/index.ts')],
+  bundle: true,
+  platform: 'node',
+  external: [],
+  outdir: 'dist',
+  minify: args.minify,
 
-    format: 'cjs',
-  }),
+  format: 'esm',
 
-  esbuild.context({
-    entryPoints: [path.resolve(__dirname, '../src/index.ts')],
-    bundle: true,
-    platform: 'node',
-    external: [],
-    outdir: 'dist',
-    minify: args.minify,
-
-    format: 'esm',
-  }),
-])
+  plugins: [
+    nodeExternalsPlugin(),
+    {
+      name: 'generate-types',
+      async setup(build) {
+        build.onEnd(async (result) => {
+          // Call the tsc command to generate the types
+          spawnSync(
+            'tsc',
+            ['--emitDeclarationOnly', '--outDir', path.resolve(__dirname, '../dist')],
+            {
+              stdio: 'inherit',
+            }
+          )
+        })
+      },
+    },
+  ],
+})
 
 console.log('- Building')
-await Promise.all(builds.map((build) => build.rebuild()))
+await build.rebuild()
 
 if (args.watch) {
   console.log('- Watching')
-  await Promise.all(builds.map((build) => build.watch()))
+  await build.watch()
 } else {
   console.log('- Cleaning up')
-  await Promise.all(builds.map((build) => build.dispose()))
+  await build.dispose()
 }
