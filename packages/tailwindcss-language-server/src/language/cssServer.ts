c846d29c3c5f04b044dce76a3a797f68703efaf1
Jordan Pittman <jordan@cryptica.me>
2024-03-04 09:34:12 -0500

    wip

--- packages/tailwindcss-language-server/src/language/cssServer.ts
+++ packages/tailwindcss-language-server/src/language/cssServer.ts
@@ -393,7 +393,7 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
     .filter((diagnostic) => {
       if (
         diagnostic.code === 'unknownAtRules' &&
-        /Unknown at rule @(tailwind|apply|config)/.test(diagnostic.message)
+        /Unknown at rule @(tailwind|apply|config|theme)/.test(diagnostic.message)
       ) {
         return false
       }
