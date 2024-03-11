853b0711a58ca0e02306f528383541a03b74cb7d
Jordan Pittman <jordan@cryptica.me>
2024-03-11 17:04:44 -0400

    Optimize code

--- packages/tailwindcss-language-service/src/util/braceLevel.ts
+++ packages/tailwindcss-language-service/src/util/braceLevel.ts
@@ -1,14 +1,11 @@
 export default function braceLevel(text: string) {
   let count = 0
+
   for (let i = text.length - 1; i >= 0; i--) {
-    switch (text[i]) {
-      case '{':
-        count += 1
-        break
-      case '}':
-        count -= 1
-        break
-    }
+    let char = text.charCodeAt(i)
+
+    count += Number(char === 0x7b /* { */) - Number(char === 0x7d /* } */)
   }
+
   return count
 }
