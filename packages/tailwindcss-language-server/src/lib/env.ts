f6db3a629bd1c68f3648f629951ec15405cf9dc0
Jordan Pittman <jordan@cryptica.me>
2024-06-27 10:28:02 -0400

    Fix loading on old versions of VSCode

--- packages/tailwindcss-language-server/src/lib/env.ts
+++ packages/tailwindcss-language-server/src/lib/env.ts
@@ -1,5 +1,4 @@
 import Module from 'node:module'
-import { isBuiltin } from 'node:module'
 import * as path from 'node:path'
 import resolveFrom from '../util/resolveFrom'
 
@@ -7,6 +6,18 @@ process.env.TAILWIND_MODE = 'build'
 process.env.TAILWIND_DISABLE_TOUCH = 'true'
 
 let oldResolveFilename = (Module as any)._resolveFilename
+
+function isBuiltin(id: string) {
+  // Node 16.17+, v18.6.0+, >= v20
+  // VSCode >= 1.78
+  if ('isBuiltin' in Module) {
+    return Module.isBuiltin(id)
+  }
+
+  // Older versions of Node and VSCode
+  // @ts-ignore
+  return Module.builtinModules.includes(id.replace(/^node:/, ''))
+}
 
 ;(Module as any)._resolveFilename = (id: any, parent: any) => {
   if (typeof id === 'string' && isBuiltin(id)) {
