e7e9eb7376f0a74b37821118de368a3e25643613
Jordan Pittman <jordan@cryptica.me>
2024-06-28 17:38:17 -0400

    Normalize CSS config file path

--- packages/tailwindcss-language-server/src/tw.ts
+++ packages/tailwindcss-language-server/src/tw.ts
@@ -77,7 +77,9 @@ async function getConfigFileFromCssFile(cssFile: string): Promise<string | null>
   if (!match) {
     return null
   }
-  return normalizePath(path.resolve(path.dirname(cssFile), match.groups.config.slice(1, -1)))
+  return normalizeDriveLetter(
+    normalizePath(path.resolve(path.dirname(cssFile), match.groups.config.slice(1, -1))),
+  )
 }
 
 export class TW {
@@ -334,7 +336,11 @@ export class TW {
 
         let isCssFile = isCssMatcher(`**/${CSS_GLOB}`)
         if (isCssFile && change.type !== FileChangeType.Deleted) {
-          let configPath = await getConfigFileFromCssFile(change.file)
+          // TODO: Determine if we can only use `normalizedFilename`
+          let configPath =
+            (await getConfigFileFromCssFile(normalizedFilename)) ||
+            (await getConfigFileFromCssFile(change.file))
+
           if (
             cssFileConfigMap.has(normalizedFilename) &&
             cssFileConfigMap.get(normalizedFilename) !== configPath
