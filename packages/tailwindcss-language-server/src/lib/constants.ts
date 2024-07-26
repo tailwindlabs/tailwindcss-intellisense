c9a2effc6d04b6d9359218e91b02b03b4a8903cc
Erlend Seppola <erlend.seppola@gmail.com>
2024-07-26 18:22:24 +0200

    fix: add mts and cts files to CONFIG_GLOB

--- packages/tailwindcss-language-server/src/lib/constants.ts
+++ packages/tailwindcss-language-server/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const CONFIG_GLOB =
-  '{tailwind,tailwind.config,tailwind.*.config,tailwind.config.*}.{js,cjs,ts,mjs}'
-export const PACKAGE_LOCK_GLOB = '{package-lock.json,yarn.lock,pnpm-lock.yaml}'
-export const CSS_GLOB = '*.{css,scss,sass,less,pcss}'
+	"{tailwind,tailwind.config,tailwind.*.config,tailwind.config.*}.{js,cjs,ts,mjs,mts,cts}";
+export const PACKAGE_LOCK_GLOB = "{package-lock.json,yarn.lock,pnpm-lock.yaml}";
+export const CSS_GLOB = "*.{css,scss,sass,less,pcss}";
