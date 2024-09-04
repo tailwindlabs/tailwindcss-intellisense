5017e99a896a8d5f6624e142ba4e097fd45bb099
Jordan Pittman <jordan@cryptica.me>
2024-09-04 10:54:32 -0400

    Add v4 document link tests for `@config`

--- packages/tailwindcss-language-server/tests/document-links/document-links.test.js
+++ packages/tailwindcss-language-server/tests/document-links/document-links.test.js
@@ -40,3 +40,42 @@ withFixture('basic', (c) => {
     ],
   })
 })
+
+withFixture('v4/basic', (c) => {
+  async function testDocumentLinks(name, { text, lang, expected }) {
+    test.concurrent(name, async ({ expect }) => {
+      let textDocument = await c.openDocument({ text, lang })
+      let res = await c.sendRequest('textDocument/documentLink', {
+        textDocument,
+      })
+
+      expect(res).toEqual(expected)
+    })
+  }
+
+  testDocumentLinks('config: file exists', {
+    text: '@config "tailwind.config.js";',
+    lang: 'css',
+    expected: [
+      {
+        target: `file://${path
+          .resolve('./tests/fixtures/v4/basic/tailwind.config.js')
+          .replace(/@/g, '%40')}`,
+        range: { start: { line: 0, character: 8 }, end: { line: 0, character: 28 } },
+      },
+    ],
+  })
+
+  testDocumentLinks('config: file does not exist', {
+    text: '@config "does-not-exist.js";',
+    lang: 'css',
+    expected: [
+      {
+        target: `file://${path
+          .resolve('./tests/fixtures/v4/basic/does-not-exist.js')
+          .replace(/@/g, '%40')}`,
+        range: { start: { line: 0, character: 8 }, end: { line: 0, character: 27 } },
+      },
+    ],
+  })
+})
