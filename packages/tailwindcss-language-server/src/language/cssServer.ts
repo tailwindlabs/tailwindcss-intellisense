b10aba16faec5887ea645baba1434bb39e34d652
Jordan Pittman <jordan@cryptica.me>
2024-11-22 15:27:28 -0500

    Fix errors around using `@import` with `layer()` and `@layer` in Tailwind CSS Language mode (#1097)
    Fixes #1095
    Fixes #1096
    Fixes #1098

--- packages/tailwindcss-language-server/src/language/cssServer.ts
+++ packages/tailwindcss-language-server/src/language/cssServer.ts
@@ -337,22 +337,29 @@ function replace(delta = 0) {
 
 function createVirtualCssDocument(textDocument: TextDocument): TextDocument {
   let content = textDocument
-      .getText()
-      .replace(/@screen(\s+[^{]+){/g, replace(-2))
-      .replace(/@variants(\s+[^{]+){/g, replace())
-      .replace(/@responsive(\s*){/g, replace())
-      .replace(/@layer(\s+[^{]{2,}){/g, replace(-3))
-      .replace(
-        /@media(\s+screen\s*\([^)]+\))/g,
-        (_match, screen) => `@media (${MEDIA_MARKER})${' '.repeat(screen.length - 4)}`,
-      )
+    .getText()
+
+    // Remove inline `@layer` directives
+    // TODO: This should be unnecessary once we have updated the bundled CSS
+    // language service
+    .replace(/@layer\s+[^;{]+(;|$)/g, '')
+
+    .replace(/@screen(\s+[^{]+){/g, replace(-2))
+    .replace(/@variants(\s+[^{]+){/g, replace())
+    .replace(/@responsive(\s*){/g, replace())
+    .replace(/@layer(\s+[^{]{2,}){/g, replace(-3))
+    .replace(
+      /@media(\s+screen\s*\([^)]+\))/g,
+      (_match, screen) => `@media (${MEDIA_MARKER})${' '.repeat(screen.length - 4)}`,
+    )
+    .replace(/@import\s*("(?:[^"]+)"|'(?:[^']+)')\s*(.*?)(?=;|$)/g, (_match, url, other) => {
       // Remove`source(…)`, `theme(…)`, and `prefix(…)` from `@import`s
       // otherwise we'll show syntax-error diagnostics which we don't want
-      .replace(
-        /@import\s*("(?:[^"]+)"|'(?:[^']+)')\s*((source|theme|prefix)\([^)]+\)\s*)+/g,
-        (_match, url) => `@import "${url.slice(1, -1)}"`,
-      )
-      .replace(/(?<=\b(?:theme|config)\([^)]*)[.[\]]/g, '_')
+      other = other.replace(/((source|theme|prefix)\([^)]+\)\s*)+?/g, '')
+
+      return `@import "${url.slice(1, -1)}" ${other}`
+    })
+    .replace(/(?<=\b(?:theme|config)\([^)]*)[.[\]]/g, '_')
 
   return TextDocument.create(
     textDocument.uri,
@@ -389,7 +396,9 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
     .filter((diagnostic) => {
       if (
         diagnostic.code === 'unknownAtRules' &&
-        /Unknown at rule @(tailwind|apply|config|theme|plugin|source|utility|variant)/.test(diagnostic.message)
+        /Unknown at rule @(tailwind|apply|config|theme|plugin|source|utility|variant)/.test(
+          diagnostic.message,
+        )
       ) {
         return false
       }
