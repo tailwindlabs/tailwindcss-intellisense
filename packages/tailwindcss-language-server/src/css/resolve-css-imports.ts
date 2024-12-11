a5b5ce82818de0bd759028eafc043d4668e1963a
Jordan Pittman <jordan@cryptica.me>
2024-12-11 12:24:58 -0500

    Recover from missing imports in the project locator

--- packages/tailwindcss-language-server/src/css/resolve-css-imports.ts
+++ packages/tailwindcss-language-server/src/css/resolve-css-imports.ts
@@ -11,7 +11,22 @@ const resolver = createResolver({
 
 const resolveImports = postcss([
   postcssImport({
-    resolve: (id, base) => resolveCssFrom(base, id),
+    resolve(id, base) {
+      try {
+        return resolveCssFrom(base, id)
+      } catch (e) {
+        // TODO: Need to test this on windows
+        return `/virtual:missing/${id}`
+      }
+    },
+
+    load(filepath) {
+      if (filepath.startsWith('/virtual:missing/')) {
+        return Promise.resolve('')
+      }
+
+      return fs.readFile(filepath, 'utf-8')
+    },
   }),
   fixRelativePaths(),
 ])
