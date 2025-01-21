dc6091d689efced60b60a0bee74f3bc38fd851ef
Jordan Pittman <jordan@cryptica.me>
2025-01-21 11:49:49 -0500

    Update tests

--- packages/tailwindcss-language-server/tests/completions/completions.test.js
+++ packages/tailwindcss-language-server/tests/completions/completions.test.js
@@ -310,7 +310,7 @@ withFixture('v4/basic', (c) => {
     let result = await completion({ lang, text, position, settings })
     let textEdit = expect.objectContaining({ range: { start: position, end: position } })
 
-    expect(result.items.length).toBe(12309)
+    expect(result.items.length).toBe(12314)
     expect(result.items.filter((item) => item.label.endsWith(':')).length).toBe(304)
     expect(result).toEqual({
       isIncomplete: false,
