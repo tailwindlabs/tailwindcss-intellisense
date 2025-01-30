5fc1528ba6e7bcac3cfb98df7195a1c5b2b73957
Jordan Pittman <jordan@cryptica.me>
2025-01-30 11:11:49 -0500

    wip
    Add support for v4 fallback

--- packages/tailwindcss-language-server/src/project-locator.test.ts
+++ packages/tailwindcss-language-server/src/project-locator.test.ts
@@ -1,5 +1,6 @@
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 import * as path from 'node:path'
+import { defineTest, js, Storage, TestUtils } from './testing'
 import { ProjectLocator } from './project-locator'
 import { URL, fileURLToPath } from 'url'
 import { Settings } from '@tailwindcss/language-service/src/util/state'
@@ -226,4 +227,79 @@ function testFixture(fixture: string, details: any[]) {
 
     expect(projects).toHaveLength(details.length)
   })
+}
+
+// ---
+
+function testLocator({ name, fs, expected }: { name: string; fs: Storage; expected: any[] }) {
+  defineTest({
+    name,
+    fs,
+    prepare,
+    async handle({ search }) {
+      let projects = await search()
+
+      let details = projects.map((project) => ({
+        version: project.tailwind.isDefaultVersion
+          ? `${project.tailwind.version} (bundled)`
+          : project.tailwind.version,
+        config: project.config.path,
+        content: project.documentSelector
+          .filter((selector) => selector.priority === 1 /** content */)
+          .map((selector) => selector.pattern)
+          .sort(),
+        selectors: project.documentSelector.map((selector) => selector.pattern).sort(),
+      }))
+
+      expect(details).toMatchObject(expected)
+    },
+  })
+}
+
+async function prepare({ root }: TestUtils) {
+  let settings = {
+    tailwindCSS: {
+      files: {
+        exclude: [],
+      },
+    },
+  } as Settings
+
+  let resolver = await createResolver({ root, tsconfig: true })
+  let locator = new ProjectLocator(root, settings, resolver)
+
+  function adjustPath(filepath: string) {
+    filepath = filepath.replace(root, '{URL}')
+
+    if (filepath.startsWith('{URL}/')) {
+      filepath = filepath.slice(5)
+    }
+
+    return filepath
+  }
+
+  async function search() {
+    let projects = await locator.search()
+
+    // Normalize all the paths for easier testing
+    for (let project of projects) {
+      project.folder = adjustPath(project.folder)
+      project.configPath = adjustPath(project.configPath)
+
+      // Config data
+      project.config.path = adjustPath(project.config.path)
+      project.config.packageRoot = adjustPath(project.config.packageRoot)
+      for (let entry of project.config.entries) {
+        entry.path = adjustPath(entry.path)
+      }
+
+      for (let selector of project.documentSelector ?? []) {
+        selector.pattern = adjustPath(selector.pattern)
+      }
+    }
+
+    return projects
+  }
+
+  return { search }
 }
