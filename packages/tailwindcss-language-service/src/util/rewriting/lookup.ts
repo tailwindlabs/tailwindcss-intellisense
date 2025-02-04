11ec70de9799253b05f0a047ee6d523c2edeba86
Jordan Pittman <jordan@cryptica.me>
2025-02-04 13:16:47 -0500

    Handle theme prefixes when looking up CSS variable values

--- packages/tailwindcss-language-service/src/util/rewriting/lookup.ts
+++ packages/tailwindcss-language-service/src/util/rewriting/lookup.ts
@@ -2,5 +2,11 @@ import { DesignSystem } from '../v4'
 
 // Resolve a variable value from the design system
 export function resolveVariableValue(design: DesignSystem, name: string) {
+  let prefix = design.theme.prefix ?? null
+
+  if (prefix && name.startsWith(`--${prefix}`)) {
+    name = `--${name.slice(prefix.length + 3)}`
+  }
+
   return design.resolveThemeValue?.(name) ?? null
 }
