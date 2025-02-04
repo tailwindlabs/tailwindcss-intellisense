bdd96054113620617eb0543e6429a33cef12e480
Avi Vahl <avi.vahl@wix.com>
2025-02-04 17:41:07 +0200

    fix(language-service): allow native esm to import package (#1122)
    with this patch, `@tailwindcss/language-service` can be imported/used in
    Node.js in native esm mode.
    
    adjusted "semver" imports to include an extension, as that package has
    no "exports".
    
    also upgrade semver to latest stable (7.7.1)
    
    ---------
    
    Co-authored-by: Jordan Pittman <jordan@cryptica.me>

--- packages/tailwindcss-language-service/src/util/semver.ts
+++ packages/tailwindcss-language-service/src/util/semver.ts
@@ -1,10 +1,11 @@
-import semverGte from 'semver/functions/gte'
-import semverLte from 'semver/functions/lte'
+import semverGte from 'semver/functions/gte.js'
+import semverLte from 'semver/functions/lte.js'
 
 export function gte(v1: string, v2: string): boolean {
   if (v1.startsWith('0.0.0-insiders')) {
     return true
   }
+
   return semverGte(v1, v2)
 }
 
@@ -12,5 +13,6 @@ export function lte(v1: string, v2: string): boolean {
   if (v1.startsWith('0.0.0-insiders')) {
     return false
   }
+
   return semverLte(v1, v2)
 }
