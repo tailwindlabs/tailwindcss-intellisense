13c82bf0bb00dd905ee0e5af8e94c7919d7e61d6
Jordan Pittman <jordan@cryptica.me>
2025-02-04 10:36:43 -0500

    Tweak formatting

--- packages/tailwindcss-language-service/src/util/semver.ts
+++ packages/tailwindcss-language-service/src/util/semver.ts
@@ -5,6 +5,7 @@ export function gte(v1: string, v2: string): boolean {
   if (v1.startsWith('0.0.0-insiders')) {
     return true
   }
+
   return semverGte(v1, v2)
 }
 
@@ -12,5 +13,6 @@ export function lte(v1: string, v2: string): boolean {
   if (v1.startsWith('0.0.0-insiders')) {
     return false
   }
+
   return semverLte(v1, v2)
 }
