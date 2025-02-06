89199df69943507988f0b550e25a447ccfcd674b
Jordan Pittman <jordan@cryptica.me>
2025-02-05 21:09:16 -0500

    Collect `Scope` metadata about document content
    Here we introduce the concept of a `Scope` which is an abstract marker for a region of content in a document. A `Scope` can be used to mark a region of text with specific metadata that can be used to drive various features in the editor. For example, a `Scope` might be used to represent things like whether a region of text is HTML or CSS; whether or not we're inside a comment, string literal, or other language-specific construct; or whether or not we're looking at a class list (regardless of the language), etc…

---
+++ packages/tailwindcss-language-service/src/scopes/analyze.ts
@@ -0,0 +1,57 @@
+import type { TextDocument } from 'vscode-languageserver-textdocument'
+import type { State } from '../util/state'
+import { type Scope, Scopes } from './scope'
+import { getLanguageBoundaries, LanguageBoundary } from '../util/getLanguageBoundaries'
+
+export async function analyzeDocument(state: State, doc: TextDocument): Promise<Scopes> {
+  let scopes: Scope[] = []
+
+  let boundaries = getLanguageBoundaries(state, doc)
+  if (boundaries === null) {
+    boundaries = [
+      {
+        type: doc.languageId,
+        span: [0, doc.getText().length],
+        range: { start: doc.positionAt(0), end: doc.positionAt(doc.getText().length) },
+      },
+    ]
+  }
+
+  for (let boundary of boundaries) {
+    for await (let scope of analyzeContexts(state, doc, boundary)) {
+      scopes.push(scope)
+    }
+  }
+
+  return new Scopes(scopes)
+}
+
+/**
+ * Figure out what kind of text we're looking at
+ */
+async function* analyzeContexts(
+  state: State,
+  doc: TextDocument,
+  boundary: LanguageBoundary,
+): AsyncIterable<Scope> {
+  if (boundary.type === 'html') {
+    yield {
+      kind: 'context.html',
+      span: boundary.span,
+    }
+  }
+
+  if (boundary.type === 'js' || boundary.type === 'jsx') {
+    yield {
+      kind: 'context.js',
+      span: boundary.span,
+    }
+  }
+
+  if (boundary.type === 'css') {
+    yield {
+      kind: 'context.css',
+      span: boundary.span,
+    }
+  }
+}
---
+++ packages/tailwindcss-language-service/src/scopes/scope.ts
@@ -0,0 +1,124 @@
+import type { Span } from '../util/state'
+
+export type ScopeKind =
+  // Marks a span of text as being a specific kind of text
+  // For example,
+  // - `context.html` marks a span of text as being HTML
+  // - `context.js` marks a span of text as being JavaScript / JSX
+  // - `context.css` marks a span of text as being CSS
+  | 'context.html'
+  | 'context.js'
+  | 'context.css'
+
+/**
+ * Represents information about a span of text in a file
+ */
+export interface Scope {
+  /** The kind of scope we represent */
+  kind: ScopeKind
+
+  /** The range during which this scope is active, inclusive */
+  span: Span
+}
+
+/**
+ * Represents a collection of scope information about some text
+ */
+export class Scopes {
+  /**
+   * This is a sorted list of scopes that are contained within the text
+   *
+   * The list is sorted in ascending order by the start of the span and then by
+   * the end of the span. This allows us to quickly find the scopes that contain
+   * a given position in the text by doing a binary search.
+   */
+  private list: Scope[]
+
+  /**
+   * Create a collection of sorted scopes
+   */
+  constructor(scopes: Scope[]) {
+    this.list = scopes.sort((a, z) => a.span[0] - z.span[0] || z.span[1] - a.span[1])
+  }
+
+  /**
+   * Get all scopes that are active at a given position in the text
+   *
+   * For example, given this position in some HTML:
+   * ```html
+   * <div class="bg-blue-500 text-white">
+   *                ^
+   * ```
+   *
+   * We know the following scopes are active:
+   * - `context.html` [0, 36]
+   * - `class.list` [12, 34]
+   * - `class.name` [12, 23]
+   */
+  public at(pos: number): Scope[] {
+    return this.between([pos, pos])
+  }
+
+  /**
+   * Get all scopes that are active for an entire range of text.
+   *
+   * Note that this does **NOT** include scopes that begin or end within the
+   * range since those scopes were not active for all positions in the range.
+   *
+   * For example, given this range in some HTML:
+   * ```html
+   * <div class="bg-blue-500 text-white">
+   *                 ^^^^^^^^^^^^
+   * ```
+   *
+   * We know the following scopes are active:
+   * - `context.html` [0, 36]
+   * - `class.list` [12, 34]
+   *
+   * Notably, `class.name` is not included because while there are `class.name`
+   * scopes that are active at the start and end of the range, they are unique
+   * scopes and neither are active for the entire range.
+   */
+  public between(span: Span): Scope[] {
+    return this.list.filter((scope) => scope.span[0] <= span[0] && span[1] <= scope.span[1])
+  }
+
+  /**
+   * Return an ordered list of all scopes applied to the text
+   */
+  public all(): Scope[] {
+    return this.list
+  }
+
+  /**
+   * Return an ordered list of all scopes applied to the text
+   */
+  public description(source?: string): string {
+    let max = 0
+    for (let scope of this.list) {
+      max = Math.max(max, scope.span[1])
+    }
+
+    let pad = max.toString().length
+
+    let lines = this.list.map((scope) => {
+      let range = `${scope.span[0].toString().padStart(pad)}, ${scope.span[1].toString().padStart(pad)}`
+      let desc = `[${range}] ${scope.kind}`
+
+      if (source) {
+        let slice = source.slice(scope.span[0], scope.span[1])
+        if (slice.length > 20) {
+          slice = slice.slice(0, 20) + '...'
+        }
+
+        slice = slice.replaceAll('\n', '\\n')
+
+        desc += ` "${slice}"`
+      }
+
+      return desc
+    })
+
+    return `\n${lines.join('\n')}\n`
+  }
+}
