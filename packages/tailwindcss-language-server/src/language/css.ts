c8d3d7af4197e13861411448baa142abf8a6f99c
Jordan Pittman <jordan@cryptica.me>
2025-02-20 16:56:33 -0500

    Refactor CSS language server setup

---
+++ packages/tailwindcss-language-server/src/language/css.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import { createConnection, ProposedFeatures } from 'vscode-languageserver/node'
+import { interceptLogs } from '../util/logs'
+import { CssServer } from './cssServer'
+
+let connection = createConnection(ProposedFeatures.all)
+
+interceptLogs(console, connection)
+
+process.on('unhandledRejection', (e: any) => {
+  console.error('Unhandled exception', e)
+})
+
+let server = new CssServer(connection)
+server.setup()
+server.listen()
