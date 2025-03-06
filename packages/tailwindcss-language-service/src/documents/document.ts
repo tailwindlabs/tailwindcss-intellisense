6161358d0c0e5f9ecc15d1c9a028c475395dee2f
Jordan Pittman <jordan@cryptica.me>
2025-03-05 19:46:36 -0500

    virtual doc scopes

--- packages/tailwindcss-language-service/src/documents/document.ts
+++ packages/tailwindcss-language-service/src/documents/document.ts
@@ -1,8 +1,10 @@
 import type { Position, TextDocument } from 'vscode-languageserver-textdocument'
-import type { State } from '../util/state'
+import type { Span, State } from '../util/state'
+import { ScopeTree } from '../scopes/tree'
+import { ScopePath, walkScope } from '../scopes/walk'
+import { ScopeContext } from '../scopes/scope'
 import { LanguageBoundary } from '../util/getLanguageBoundaries'
 import { isWithinRange } from '../util/isWithinRange'
-import { getTextWithoutComments } from '../util/doc'
 
 export type DocumentContext = 'html' | 'css' | 'js' | 'other' | (string & {})
 
@@ -29,33 +31,56 @@ export class VirtualDocument {
    */
   private boundaries: LanguageBoundary[]
 
+  /**
+   * A description of this document's "interesting" parts
+   *
+   * This is used to determine how the document is structured and what parts
+   * are relevant to the current operation.
+   *
+   * For example, we can use this to get all class lists in a document, to
+   * determine if the cursor is in a class name, or what part of a document is
+   * considered "CSS" inside HTML or Vue documents.
+   */
+  private scopes: ScopeTree
+
   constructor(
     state: () => State,
     storage: TextDocument,
     boundaries: LanguageBoundary[],
+    scopes: ScopeTree,
   ) {
     this.state = state
     this.storage = storage
     this.boundaries = boundaries
+    this.scopes = scopes
   }
 
   /**
    * The current content of the document
    */
   public get contents() {
+    let spans: Span[] = []
+
+    walkScope(this.scopes.all(), {
+      enter(node) {
+        if (node.kind !== 'comment') return
+        spans.push(node.source.scope)
+      },
+    })
+
+    // TODO: Drop comment removal once all features only query scopes
+    let text = this.storage.getText()
+
     // Replace all comment nodes with whitespace
     let tmp = ''
-
-    let type = this.boundaries[0].type
-    if (type === 'html') {
-      tmp = getTextWithoutComments(this.storage, 'html')
-    } else if (type === 'css') {
-      tmp = getTextWithoutComments(this.storage, 'css')
-    } else if (type === 'js') {
-      tmp = getTextWithoutComments(this.storage, 'js')
-    } else if (type === 'jsx') {
-      tmp = getTextWithoutComments(this.storage, 'jsx')
+    let last = 0
+    for (let [start, end] of spans) {
+      tmp += text.slice(last, start)
+      tmp += text.slice(start, end).replace(/./gs, (char) => (char === '\n' ? '\n' : ' '))
+      last = end
     }
+
+    tmp += text.slice(last)
 
     return tmp
   }
@@ -71,5 +96,22 @@ export class VirtualDocument {
     }
 
     return null
+  }
+
+  /**
+   * Find the inner-most scope containing a given cursor position.
+   */
+  public scopeAt(cursor: Position): ScopePath {
+    return this.scopes.at(this.storage.offsetAt(cursor))
+  }
+
+  /**
+   * Find the inner-most scope containing a given cursor position.
+   */
+  public contextAt(cursor: Position): ScopeContext {
+    let scope = this.scopes.closestAt('context', this.storage.offsetAt(cursor))
+    if (!scope) throw new Error('Unreachable')
+
+    return scope
   }
 }
