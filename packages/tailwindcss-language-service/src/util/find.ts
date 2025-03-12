94f130039252a04dd3f4f77dd07f27c513cbf210
Jordan Pittman <jordan@cryptica.me>
2025-03-12 11:50:35 -0400

    wip

--- packages/tailwindcss-language-service/src/util/find.ts
+++ packages/tailwindcss-language-service/src/util/find.ts
@@ -192,13 +192,13 @@ export function matchClassFunctions(text: string, fnNames: string[]): RegExpMatc
 }
 
 export async function findClassListsInHtmlRange(
-  // PickDeep from 'type-fest' package cannot be used here due to a circular reference issue in the package
-  // Fixes are underway (see: https://github.com/sindresorhus/type-fest/pull/1079)
-  state: { editor?: Pick<NonNullable<State['editor']>, 'getConfiguration'> },
+  state: State,
   doc: TextDocument,
   type: 'html' | 'js' | 'jsx',
   range?: Range,
 ): Promise<DocumentClassList[]> {
+  if (!state.editor) return []
+
   const text = getTextWithoutComments(doc, type, range)
 
   const settings = (await state.editor.getConfiguration(doc.uri)).tailwindCSS
