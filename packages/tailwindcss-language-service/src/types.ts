9c9b63a438046da0827f08f4f3a7704bdf755b3b
Laurynas Grigutis <laurynas.grigutis@barbora.lt>
2025-03-16 16:28:26 +0200

    fix: DeepPartial type issues

--- packages/tailwindcss-language-service/src/types.ts
+++ packages/tailwindcss-language-service/src/types.ts
@@ -1,8 +1,8 @@
 export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends (infer U)[]
-    ? U[]
-    : T[P] extends (...args: any) => any
-      ? T[P] | undefined
+  [P in keyof T]?: T[P] extends ((...args: any) => any) | ReadonlyArray<any> | Date
+    ? T[P]
+    : T[P] extends (infer U)[]
+      ? U[]
       : T[P] extends object
         ? DeepPartial<T[P]>
         : T[P]
