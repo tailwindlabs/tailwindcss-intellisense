44ba4b9bf9f188c01c2f1507ca7099d534fdc1a7
Jordan Pittman <jordan@cryptica.me>
2025-03-16 15:23:47 -0400

    Rename config file

--- packages/tailwindcss-language-service/scripts/build.mjs
+++ packages/tailwindcss-language-service/scripts/build.mjs
@@ -30,7 +30,7 @@ let build = await esbuild.context({
           // Call the tsc command to generate the types
           spawnSync(
             'tsc',
-            ['-p', path.resolve(__dirname, './type-gen.tsconfig.json'), '--emitDeclarationOnly', '--outDir', path.resolve(__dirname, '../dist')],
+            ['-p', path.resolve(__dirname, './tsconfig.build.json'), '--emitDeclarationOnly', '--outDir', path.resolve(__dirname, '../dist')],
             {
               stdio: 'inherit',
             },
