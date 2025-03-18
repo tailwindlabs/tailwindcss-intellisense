2bb7a5bb9254adab89a3e2b59fdddcacb779decd
Jordan Pittman <jordan@cryptica.me>
2025-03-18 10:20:15 -0400

    Re-implement project search with fdir

--- packages/tailwindcss-language-server/src/glob.ts
+++ packages/tailwindcss-language-server/src/glob.ts
@@ -1,5 +1,5 @@
-import glob from 'fast-glob'
-import os from 'node:os'
+import { fdir } from 'fdir'
+import picomatch from 'picomatch'
 
 interface SearchFilesOptions {
   /** The directory to search in */
@@ -13,15 +13,51 @@ interface SearchFilesOptions {
 }
 
 export async function searchFiles(opts: SearchFilesOptions) {
-  let results = await glob(opts.include, {
-    cwd: opts.root,
-    ignore: opts.exclude,
-    onlyFiles: true,
-    absolute: true,
+  // Ignore patterns in the project search are generally intended to match
+  // directories but _may_ also match files.
+  //
+  // A pattern like `**/node_modules` should match like these:
+  // - `**/node_modules/` to exclude directories from traversal
+  // - `**/node_modules/**/*` to exclude files from results
+  //
+  // This approximately matches how fast-glob's `ignore` option works.
+  let shouldIgnore = picomatch(
+    [...new Set(opts.exclude.flatMap((x) => [x, `${x}/`, `${x}/**/*`]))],
+    { dot: true },
+  )
+
+  let crawler = new fdir({
+    // onlyFiles: true
+    includeDirs: false,
+    excludeFiles: false,
+
+    // absolute: true
+    relativePaths: false,
+    resolvePaths: false,
+    includeBasePath: true,
+
+    // followSymbolicLinks: true
+    resolveSymlinks: false,
+    excludeSymlinks: false,
+
+    //
     suppressErrors: true,
-    dot: true,
-    concurrency: Math.max(os.cpus().length, 1),
+
+    // Normalize Windows paths to use forward slashes
+    pathSeparator: '/',
+
+    // Don't return files that are ignored
+    filters: [(path) => !shouldIgnore(path)],
+
+    // Don't traverse into directories that are ignored
+    exclude: (_, path) => shouldIgnore(path),
+
+    globFunction: picomatch,
   })
+
+  crawler = crawler.globWithOptions(opts.include, { dot: true })
+
+  let results = await crawler.crawl(opts.root).withPromise()
 
   return results
 }
