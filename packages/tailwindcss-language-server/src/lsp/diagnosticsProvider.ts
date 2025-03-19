a4d2549aadb9f62a5bc093208eec967d613117bd
Jordan Pittman <jordan@cryptica.me>
2025-03-18 22:22:24 -0400

    Refactor language service
    This will set us up for more direct, language-service specific testing.
    
    This is very much a work in progress but the ultimate goal is for the majority of language server tests to be able to run against both the language service _and_ language server

--- packages/tailwindcss-language-server/src/lsp/diagnosticsProvider.ts
+++ packages/tailwindcss-language-server/src/lsp/diagnosticsProvider.ts
@@ -1,22 +1,18 @@
 import type { TextDocument } from 'vscode-languageserver-textdocument'
 import type { State } from '@tailwindcss/language-service/src/util/state'
-import { doValidate } from '@tailwindcss/language-service/src/diagnostics/diagnosticsProvider'
-import isExcluded from '../util/isExcluded'
+import type { LanguageService } from '@tailwindcss/language-service/src/service'
 
-export async function provideDiagnostics(state: State, document: TextDocument) {
-  if (await isExcluded(state, document)) {
-    clearDiagnostics(state, document)
-  } else {
-    state.editor?.connection.sendDiagnostics({
-      uri: document.uri,
-      diagnostics: await doValidate(state, document),
-    })
-  }
-}
+export async function provideDiagnostics(
+  service: LanguageService,
+  state: State,
+  document: TextDocument,
+) {
+  if (!state.enabled) return
+  let doc = await service.open(document.uri)
+  let diagnostics = await doc?.diagnostics()
 
-export function clearDiagnostics(state: State, document: TextDocument): void {
   state.editor?.connection.sendDiagnostics({
     uri: document.uri,
-    diagnostics: [],
+    diagnostics: diagnostics ?? [],
   })
 }
