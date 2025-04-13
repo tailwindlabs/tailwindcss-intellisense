2f92433b91b92b4a028fa71a3c6b4ca4c7717a06
Jordan Pittman <jordan@cryptica.me>
2025-04-13 10:54:05 -0400

    Represent language service diagnostics using the pull model
    Protocol v3.17 added support for pull-model diagnostics which are modeled as a “report” that is either a full set of diagnostics for a document (and related documents) or “unchanged”
    
    We can convert these to the push model by pulling the diagnostics like we were doing before and inspecting the report before pushing diagnostics to the client

--- packages/tailwindcss-language-server/src/lsp/diagnosticsProvider.ts
+++ packages/tailwindcss-language-server/src/lsp/diagnosticsProvider.ts
@@ -9,10 +9,13 @@ export async function provideDiagnostics(
 ) {
   if (!state.enabled) return
   let doc = await service.open(document.uri)
-  let diagnostics = await doc?.diagnostics()
+  let report = await doc?.diagnostics()
+
+  // No need to send diagnostics if the document is unchanged
+  if (report.kind === 'unchanged') return
 
   state.editor?.connection.sendDiagnostics({
     uri: document.uri,
-    diagnostics: diagnostics ?? [],
+    diagnostics: report?.items ?? [],
   })
 }
