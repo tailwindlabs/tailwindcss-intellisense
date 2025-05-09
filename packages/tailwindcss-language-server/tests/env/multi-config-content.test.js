38d7a12f8d088e297470aa427db7f3d02317ea6f
Jordan Pittman <jordan@cryptica.me>
2025-05-09 19:28:14 -0400

    Rewrite test

--- packages/tailwindcss-language-server/tests/env/multi-config-content.test.js
+++ packages/tailwindcss-language-server/tests/env/multi-config-content.test.js
@@ -1,38 +1,85 @@
-import { test } from 'vitest'
-import { withFixture } from '../common'
+import { expect } from 'vitest'
+import { css, defineTest, html, js, json, symlinkTo } from '../../src/testing'
+import dedent from 'dedent'
+import { createClient } from '../utils/client'
 
-withFixture('multi-config-content', (c) => {
-  test.concurrent('multi-config with content config - 1', async ({ expect }) => {
-    let textDocument = await c.openDocument({ text: '<div class="bg-foo">', dir: 'one' })
-    let res = await c.sendRequest('textDocument/hover', {
-      textDocument,
-      position: { line: 0, character: 13 },
+defineTest({
+  name: 'multi-config with content config',
+  fs: {
+    'tailwind.config.one.js': js`
+      module.exports = {
+        content: ['./one/**/*'],
+        theme: {
+          extend: {
+            colors: {
+              foo: 'red',
+            },
+          },
+        },
+      }
+    `,
+    'tailwind.config.two.js': js`
+      module.exports = {
+        content: ['./two/**/*'],
+        theme: {
+          extend: {
+            colors: {
+              foo: 'blue',
+            },
+          },
+        },
+      }
+    `,
+  },
+  prepare: async ({ root }) => ({ client: await createClient({ root }) }),
+  handle: async ({ client }) => {
+    let one = await client.open({
+      lang: 'html',
+      name: 'one/index.html',
+      text: '<div class="bg-foo">',
     })
 
-    expect(res).toEqual({
+    let two = await client.open({
+      lang: 'html',
+      name: 'two/index.html',
+      text: '<div class="bg-foo">',
+    })
+
+    // <div class="bg-foo">
+    //             ^
+    let hoverOne = await one.hover({ line: 0, character: 13 })
+    let hoverTwo = await two.hover({ line: 0, character: 13 })
+
+    expect(hoverOne).toEqual({
       contents: {
         language: 'css',
-        value:
-          '.bg-foo {\n  --tw-bg-opacity: 1;\n  background-color: rgb(255 0 0 / var(--tw-bg-opacity, 1)) /* #ff0000 */;\n}',
+        value: dedent`
+          .bg-foo {
+            --tw-bg-opacity: 1;
+            background-color: rgb(255 0 0 / var(--tw-bg-opacity, 1)) /* #ff0000 */;
+          }
+        `,
       },
-      range: { start: { line: 0, character: 12 }, end: { line: 0, character: 18 } },
-    })
-  })
-
-  test.concurrent('multi-config with content config - 2', async ({ expect }) => {
-    let textDocument = await c.openDocument({ text: '<div class="bg-foo">', dir: 'two' })
-    let res = await c.sendRequest('textDocument/hover', {
-      textDocument,
-      position: { line: 0, character: 13 },
+      range: {
+        start: { line: 0, character: 12 },
+        end: { line: 0, character: 18 },
+      },
     })
 
-    expect(res).toEqual({
+    expect(hoverTwo).toEqual({
       contents: {
         language: 'css',
-        value:
-          '.bg-foo {\n  --tw-bg-opacity: 1;\n  background-color: rgb(0 0 255 / var(--tw-bg-opacity, 1)) /* #0000ff */;\n}',
+        value: dedent`
+          .bg-foo {
+            --tw-bg-opacity: 1;
+            background-color: rgb(0 0 255 / var(--tw-bg-opacity, 1)) /* #0000ff */;
+          }
+        `,
       },
-      range: { start: { line: 0, character: 12 }, end: { line: 0, character: 18 } },
+      range: {
+        start: { line: 0, character: 12 },
+        end: { line: 0, character: 18 },
+      },
     })
-  })
+  },
 })
