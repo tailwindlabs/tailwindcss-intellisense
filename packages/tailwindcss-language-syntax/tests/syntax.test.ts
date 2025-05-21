29af61b28f75fc1916a5da064d89eb44436edf95
Jordan Pittman <jordan@cryptica.me>
2025-05-21 11:48:29 -0400

    wip

--- packages/tailwindcss-language-syntax/tests/syntax.test.ts
+++ packages/tailwindcss-language-syntax/tests/syntax.test.ts
@@ -51,7 +51,7 @@ test('plugins', async ({ expect }) => {
             ^      source.css.tailwind string.quoted.double.css punctuation.definition.string.begin.css
              ^^^   source.css.tailwind string.quoted.double.css
                 ^  source.css.tailwind string.quoted.double.css punctuation.definition.string.end.css
-                 ^ source.css.tailwind punctuation.terminator.rule.css
+                 ^ source.css.tailwind
 
     @plugin "foo";
     ^              source.css.tailwind keyword.control.at-rule.plugin.tailwind punctuation.definition.keyword.tailwind
@@ -60,7 +60,7 @@ test('plugins', async ({ expect }) => {
             ^      source.css.tailwind string.quoted.double.css punctuation.definition.string.begin.css
              ^^^   source.css.tailwind string.quoted.double.css
                 ^  source.css.tailwind string.quoted.double.css punctuation.definition.string.end.css
-                 ^ source.css.tailwind punctuation.terminator.rule.css
+                 ^ source.css.tailwind
 
     "
   `)
@@ -115,22 +115,24 @@ test('plugins with options', async ({ expect }) => {
     ^                      source.css.tailwind
 
     html,
-    ^^^^^^                 source.css.tailwind
+    ^^^^                   source.css.tailwind meta.selector.css entity.name.tag.css
+        ^                  source.css.tailwind meta.selector.css punctuation.separator.list.comma.css
 
     body {
-    ^^^^^                  source.css.tailwind
-         ^                 source.css.tailwind meta.at-rule.plugin.body.tailwind punctuation.section.plugin.begin.bracket.curly.tailwind
+    ^^^^                   source.css.tailwind meta.selector.css entity.name.tag.css
+        ^                  source.css.tailwind
+         ^                 source.css.tailwind meta.property-list.css punctuation.section.property-list.begin.bracket.curly.css
 
       color: red;
-    ^^                     source.css.tailwind meta.at-rule.plugin.body.tailwind
-      ^^^^^                source.css.tailwind meta.at-rule.plugin.body.tailwind meta.property-name.css support.type.property-name.css
-           ^               source.css.tailwind meta.at-rule.plugin.body.tailwind punctuation.separator.key-value.css
-            ^              source.css.tailwind meta.at-rule.plugin.body.tailwind
-             ^^^           source.css.tailwind meta.at-rule.plugin.body.tailwind meta.property-value.css support.constant.color.w3c-standard-color-name.css
-                ^          source.css.tailwind meta.at-rule.plugin.body.tailwind punctuation.terminator.rule.css
+    ^^                     source.css.tailwind meta.property-list.css
+      ^^^^^                source.css.tailwind meta.property-list.css meta.property-name.css support.type.property-name.css
+           ^               source.css.tailwind meta.property-list.css punctuation.separator.key-value.css
+            ^              source.css.tailwind meta.property-list.css
+             ^^^           source.css.tailwind meta.property-list.css meta.property-value.css support.constant.color.w3c-standard-color-name.css
+                ^          source.css.tailwind meta.property-list.css punctuation.terminator.rule.css
 
     }
-    ^                      source.css.tailwind meta.at-rule.plugin.body.tailwind punctuation.section.plugin.end.bracket.curly.tailwind
+    ^                      source.css.tailwind meta.property-list.css punctuation.section.property-list.end.bracket.curly.css
 
     "
   `)
