3d4d850976d474a5193ae8ab35cca68a9778afec
Jordan Pittman <jordan@cryptica.me>
2025-07-07 10:50:03 -0400

    tweak

--- packages/tailwindcss-language-service/src/util/color.ts
+++ packages/tailwindcss-language-service/src/util/color.ts
@@ -216,10 +216,12 @@ export function getColor(state: State, className: string): culori.Color | Keywor
 
     let color = getColorFromRoot(state, css)
 
+    let prefix = state.designSystem.theme.prefix ?? ''
+
     // TODO: Either all consumers of this API should assume there's no prefix
     // or pass in correctly prefixed classes
-    if (state.designSystem.theme.prefix !== '' && !color) {
-      className = `${state.designSystem.theme.prefix}:${className}`
+    if (prefix && !color && !className.startsWith(prefix + ':')) {
+      className = `${prefix}:${className}`
       css = state.designSystem.compile([className])[0]
       color = getColorFromRoot(state, css)
     }
