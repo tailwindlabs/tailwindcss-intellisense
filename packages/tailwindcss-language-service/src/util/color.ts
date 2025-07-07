ee77ab815bf7b2e558616cf52d5fd3fca0b2d2c8
Jordan Pittman <jordan@cryptica.me>
2025-07-07 10:36:21 -0400

    Ensure color swatches show in completions when using a prefix

--- packages/tailwindcss-language-service/src/util/color.ts
+++ packages/tailwindcss-language-service/src/util/color.ts
@@ -216,6 +216,14 @@ export function getColor(state: State, className: string): culori.Color | Keywor
 
     let color = getColorFromRoot(state, css)
 
+    // TODO: Either all consumers of this API should assume there's no prefix
+    // or pass in correctly prefixed classes
+    if (state.designSystem.theme.prefix !== '' && !color) {
+      className = `${state.designSystem.theme.prefix}:${className}`
+      css = state.designSystem.compile([className])[0]
+      color = getColorFromRoot(state, css)
+    }
+
     return color
   }
 
